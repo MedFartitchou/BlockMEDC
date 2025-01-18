adb6dbacbc5389700001b4d988f57facd66c5819
MedFartitchou <140612212+MedFartitchou@users.noreply.github.com>
2025-01-18 22:12:20 +0100

    Update Structs.sol

--- Structs.sol
+++ Structs.sol
@@ -1,7 +1,9 @@
-pragma solidity ^0.4.23;
-library SharedStructs{
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+library SharedStructs {
     // M_Authority.sol
-     struct PKI_Certificate {
+    struct PKI_Certificate {
         address identity;
         bytes32 publicKey;
         uint expiry;
@@ -10,33 +12,33 @@ library SharedStructs{
     }
 
     // University.sol
-   struct U_institution {
-        bool registered;    // registre an Institition of university 
+    struct U_institution {
+        bool registered;    // register an Institution of university
         address head;
-        address vice_head;   
+        address vice_head;
     }
     struct A_Institution {
-        bool registered;  // registre an Assistant Institution for trainer of university 
+        bool registered;  // register an Assistant Institution for trainer of university
         address head;
-        address admin;   
+        address admin;
     }
 
     // Institution.sol
     struct Student {
-        bytes32 id;    // id of student 
-        bool registered; // registre in university as student 
-        uint accomplished; // complete a degre certifacte ( Deug , licence , master )
-        mapping(bytes32 => TranscriptCer) trans; // transcript of student 
-        mapping(bytes32 => Acdiploma) diploms;  // diploma of student 
+        bytes32 id;    // id of student
+        bool registered; // register in university as student
+        uint accomplished; // complete a degree certificate (Deug, licence, master)
+        mapping(bytes32 => TranscriptCer) trans; // transcript of student
+        mapping(bytes32 => Acdiploma) diploms;  // diploma of student
     }
     struct Professor {
-        bool registered; // registre in university as student 
+        bool registered; // register in university as professor
         address Professor;
         bytes32 id;
         bytes32 Department;
     }
     struct Admin {
-        bool registered; // registre in university as student 
+        bool registered; // register in university as admin
         address Admin;
         bytes32 id;
     }
@@ -45,41 +47,37 @@ library SharedStructs{
         address issuer;
         address receiver;
         bytes32 Degre;
-        bytes32 Semstre; 
-        uint score; // date 
+        bytes32 Semstre;
+        uint score;
         uint date;
         uint note;
         bool status;
     }
 
-    
     struct Acdiploma {
-    address issuer_dean;
-    address issuer_president;  // Fixed typo from 'issuer_presedent'
-    address receiver;
-    bytes32 Degre;
-    uint date;
-    uint note;
-    bool status;
-   }
-
-    
-    // A_Institution.sol
-     struct TrStudent {
-        bytes32 id;    // id of student 
-        bool registered; // registre in Assistant Institution  as trainer student 
-        uint accomplished; // complete a specific internship program 
-        mapping(bytes32 => Prdiploma) diplomsPr;  // Prdiploma of student 
+        address issuer_dean;
+        address issuer_president;
+        address receiver;
+        bytes32 Degre;
+        uint date;
+        uint note;
+        bool status;
     }
-      struct Prdiploma {
+
+    // A_Institution.sol
+    struct TrStudent {
+        bytes32 id;    // id of student
+        bool registered; // register in Assistant Institution as trainer student
+        uint accomplished; // complete a specific internship program
+        mapping(bytes32 => Prdiploma) diplomsPr;  // Prdiploma of student
+    }
+    struct Prdiploma {
         address issuer_head;
-        address issuer_Institution ;
+        address issuer_Institution;
         address receiver;
         bytes32 Degre;
         uint period;
         bool status;
         uint date;
-    } 
-
-} 
- 
+    }
+}
